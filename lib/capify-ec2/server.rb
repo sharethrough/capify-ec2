6704ccd0974de7da4ddd571e523f0640079be1c5
Christopher Nguyen <cnguyen@sharethrough.com>
2020-10-29 17:17:03 -0700

    Fog AWS changed the way the scoping was named, so updated to the current namespace resolution: Fog::AWS::Compute::Server

--- lib/capify-ec2/server.rb
+++ lib/capify-ec2/server.rb
@@ -2,8 +2,8 @@ require 'rubygems'
 require 'fog/aws/models/compute/server'
 
 module Fog
-  module Compute
-    class AWS
+  module AWS
+    class Compute
       class Server
         def contact_point(use_private_ip = false)
           if use_private_ip
@@ -12,7 +12,6 @@ module Fog
             dns_name || public_ip_address || private_ip_address
           end
         end
-
         def name
           tags["Name"] || id
         end
